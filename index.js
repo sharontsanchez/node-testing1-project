bcc78ac76be0cf22385007df789cc3c08ed7d680
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-23 10:40:11 -0600

    isEvenNumberAsyn function is passing

--- index.js
+++ index.js
@@ -183,8 +183,9 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
+async function isEvenNumberAsync(number) {
   // ✨ implement
+ return number % 2 === 0 || false
 }
 
 module.exports = {
